5e5104f348d8e79019279c82465360b5c9113995
Megg <arnimegg@gmail.com>
2019-11-28 15:02:34 +0000

    finally figured out routing :neckbeard:

--- src/views/Lists/index.jsx
+++ src/views/Lists/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
 import ListList from '../../components/ListList';
 import data from '../../resources/data.json';
 import styles from './styles';
 
-const Lists = () => (
-	<View style={styles.main}>
-		<ListList lists={data.lists} />
-	</View>
-);
+const Lists = ( navigate ) => {
+	const { id }  = navigate.navigation.state.params
+	return (
+		<View style={styles.main}>
+			<ListList lists={data.lists.filter((list) => list.boardId === id )} />
+		</View>
+	);
+}
+
+Lists.propTypes = {
+	boardId: PropTypes.shape({
+		id: PropTypes.number.isRequired
+	}).isRequired
+};
 
 export default Lists;
