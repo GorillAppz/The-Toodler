4a7a5578ed35e4b4109760eb1bde5d19c72d7e87
Megg <arnimegg@gmail.com>
2019-11-28 02:09:35 +0000

    List Fun

--- src/components/ListList/index.jsx
+++ src/components/ListList/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, FlatList, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import ListItem from '../ListItem';
 import styles from './styles';
 
 const ListList = ({ lists }) => (
 	<View>
+		<Button
+			icon={{
+				name: 'add-circle-outline',
+				size: 20,
+				color: 'white'
+			}}
+			buttonStyle={styles.addButton}
+			title="Add List"
+		/>
 		<Text>
 			{`Board #${lists.boardId}`}
 		</Text>
--- src/components/ListList/styles.js
+++ src/components/ListList/styles.js
@@ -3,5 +3,20 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
 	container: {
 		display: 'flex'
+	},
+	addButton: {
+		backgroundColor: 'green',
+		height: 30,
+		width: 120,
+		alignItems: 'center',
+		display: 'flex',
+		alignSelf: 'flex-end',
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: '#013222',
+		margin: 10,
+		padding: 1,
+		shadowColor: '#013222',
+		shadowOpacity: 0.5
 	}
 });
