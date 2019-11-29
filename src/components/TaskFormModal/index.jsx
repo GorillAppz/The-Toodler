9c76256324f5f2893ee1df7e18b23a5ab8f2e91a
Daniel <danielevalberg@gmail.com>
2019-11-29 22:06:09 +0000

    working on task

---
+++ src/components/TaskFormModal/index.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+import { Input, Button, Text } from 'react-native-elements';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const initState = {
+	fields: {
+		name: '',
+		description: '',
+		listId: '',
+		isFinished: false
+	},
+	errors: {
+		name: '',
+		description: '',
+		color: ''
+	}
+};
+
+class TaskFormModal extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { ...initState, listId: props.listId };
+	}
+
+	setStateOnModalShow() {
+		const { prevTask, listId } = this.props;
+		if (prevTask) {
+			this.setState({ fields: { ...prevTask, listId } });
+		} else {
+			this.setState({ fields: { ...initState.fields, listId }, errors: { ...initState.errors } });
+		}
+	}
+
+	submitForm() {
+		const { submitHandler } = this.props;
+		const { fields } = this.state;
+		if (!this.validateForm()) { return; }
+		submitHandler(fields);
+	}
+
+	validateForm() {
+		const errors = {};
+		const { fields } = this.state;
+
+		if (fields.name.length < 3) {
+			errors.name = 'Name must be atleast 3 characters!';
+		}
+
+		if (fields.description.length <= 0) {
+			errors.description = 'Please enter a description';
+		}
+
+		this.setState({ errors: { ...initState.errors, ...errors } });
+
+		return !(Object.keys(errors).length > 0);
+	}
+
+	inputHandler(name, value) {
+		const { fields } = this.state;
+		this.setState({ fields: { ...fields, [name]: value } });
+	}
+
+	render() {
+		const { fields, errors } = this.state;
+		const { isVisible, cancelHandler, title } = this.props;
+
+		return (
+			<View>
+				<Modal isVisible={isVisible} onModalShow={() => this.setStateOnModalShow()}>
+					<View style={styles.modal}>
+						<Text h3 style={styles.title}>{title}</Text>
+						<Input
+							label="Name"
+							placeholder="Enter board name"
+							value={fields.name}
+							errorStyle={{ color: 'red' }}
+							errorMessage={errors.name}
+							maxLength={40}
+							onChangeText={(text) => this.inputHandler('name', text)}
+							containerStyle={styles.inputContainer}
+						/>
+
+						<Input
+							label="Description"
+							placeholder="Enter description"
+							value={fields.name}
+							errorStyle={{ color: 'red' }}
+							errorMessage={errors.name}
+							maxLength={40}
+							onChangeText={(text) => this.inputHandler('name', text)}
+							containerStyle={styles.inputContainer}
+						/>
+
+						<View style={styles.buttonsContainer}>
+							<Button title="Cancel" onPress={() => cancelHandler()} />
+							<Button
+								title="Submit"
+								buttonStyle={styles.submitButton}
+								onPress={() => this.submitForm()}
+							/>
+						</View>
+					</View>
+
+				</Modal>
+			</View>
+		);
+	}
+}
+
+export default TaskFormModal;
--- src/components/TaskItem/index.jsx
+++ src/components/TaskItem/index.jsx
@@ -3,16 +3,44 @@ import { View, Text, TouchableHighlight } from 'react-native';
 import { CheckBox } from 'react-native-elements';
 import { connect } from 'react-redux';
 import styles from './styles';
-import { deleteTask } from '../../actions/taskActions';
+import { deleteTask, updateTask } from '../../actions/taskActions';
 import OptionModal from '../OptionModal';
 import { taskType, deleteTaskType, updateIsFinishedType } from '../../types';
+import TaskFormModal from '../TaskFormModal';
 
 const TaskItem = ({ task, deleteTask, updateIsFinished }) => {
-	const [showOptions, toggleOptions] = useState(false);
+	const [activeModal, setActiveModal] = useState('');
+
+	const modalToShow = () => {
+		if (activeModal === 'option') {
+			return (
+				<OptionModal
+					title={task.name}
+					isVisible
+					deleteHandler={() => { deleteTask(task.id); setActiveModal(''); }}
+					editHandler={() => { setActiveModal('edit'); }}
+					cancelHandler={() => setActiveModal('')}
+				/>
+			);
+		}
+		if (activeModal === 'edit') {
+			return (
+				<TaskFormModal
+					isVisible
+					cancelHandler={() => setActiveModal('')}
+					submitHandler={(newTask) => { updateTask(newTask); setActiveModal(''); }}
+					prevTask={task}
+					title="Editing Task"
+				/>
+			);
+		}
+		return null;
+	};
+
 	return (
 		<View>
 			<TouchableHighlight
-				onLongPress={() => toggleOptions(true)}
+				onLongPress={() => setActiveModal('option')}
 			>
 				<View style={styles.item}>
 					<View style={styles.nameWrapper}>
@@ -22,19 +50,13 @@ const TaskItem = ({ task, deleteTask, updateIsFinished }) => {
 						<CheckBox
 							title={task.name}
 							checked={task.isFinished}
-							onLongPress={() => toggleOptions(true)}
+							onLongPress={() => setActiveModal('edit')}
 							onPress={() => updateIsFinished(task.id)}
 						/>
 					</View>
 				</View>
 			</TouchableHighlight>
-			<OptionModal
-				title={task.name}
-				isVisible={showOptions}
-				deleteHandler={() => { deleteTask(task.id); toggleOptions(false); }}
-				editHandler={() => toggleOptions(false)}
-				cancelHandler={() => toggleOptions(false)}
-			/>
+			{modalToShow()}
 		</View>
 	);
 };
