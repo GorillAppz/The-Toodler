8e1f7ef5865d42fde9b3dfc85cc76b6abce36dbd
Megg <arnimegg@gmail.com>
2019-11-29 21:47:42 +0000

    minor bug fix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,11 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Boards from '../views/Boards';
 import Lists from '../views/Lists';
-import Tasks from '../views/Tasks';
 
 const MainNavigator = createStackNavigator(
 	{
 		Boards: { screen: Boards },
-		Lists: { screen: Lists },
-		Tasks: { screen: Tasks }
+		Lists: { screen: Lists }
 	},
 	{
 		initialRouteName: 'Boards',
